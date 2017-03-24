2dbefaf544bd9f0cb5d6b8739647aadbf9c24f74
Ted Wu <ted.wu@chartboost.com>
2017-03-24 14:33:50 -0700

    salaries db from sqlite

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # restful-api
 Create a RESTFUL API for public datasets 
+
+Technologies Used:
+SqlAlchemy
+Flask RestFul API
+Flask Framework
+Python 2.7
+
--- api.py
+++ api.py
@@ -1,8 +1,6 @@
 from flask import Flask, request
 from flask_restful import Resource, Api
 from sqlalchemy import create_engine
-from flaskr import init_db
-
 
 from json import dumps
 
