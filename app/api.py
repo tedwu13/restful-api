e05eaa666d283cfcbe123efbc67049012e158984
Ted Wu <ted.wu@chartboost.com>
2017-03-23 20:15:11 -0700

    Sync salaries db

--- app/api.py
+++ app/api.py
@@ -11,18 +11,35 @@ from json import dumps
 # Start a virtual environment 
 # When you run app.py, you can curl the web address: curl http://127.0.0.1:5000 to get the data
 
-engine = create_engine('sqlite://salaries.db')
+e = create_engine('sqlite:///salaries.db')
 
 app = Flask(__name__)
 api = Api(app)
 
-class Departments(Resource):
-    def get(self):
-        # Connect to database
-        connect = engine.connect
-        # Perform the query and return JSON data
-        # query = connect.execute("SELECT ")
+e = create_engine('sqlite:///salaries.db')
 
+app = Flask(__name__)
+api = Api(app)
+
+class Departments_Meta(Resource):
+    def get(self):
+        #Connect to databse
+        conn = e.connect()
+        #Perform query and return JSON data
+        query = conn.execute("select distinct DEPARTMENT from salaries")
+        return {'departments': [i[0] for i in query.cursor.fetchall()]}
+
+class Departmental_Salary(Resource):
+    def get(self, department_name):
+        conn = e.connect()
+        query = conn.execute("select * from salaries where Department='%s'"%department_name.upper())
+        #Query the result and get cursor.Dumping that data to a JSON is looked by extension
+        result = {'data': [dict(zip(tuple (query.keys()) ,i)) for i in query.cursor]}
+        return result
+        #We can have PUT,DELETE,POST here. But in our API GET implementation is sufficient
+ 
+api.add_resource(Departmental_Salary, '/dept/<string:department_name>')
+api.add_resource(Departments_Meta, '/departments')
 
 if __name__ == '__main__':
-    app.run(debug=True)
+     app.run()
