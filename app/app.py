12354ea3ba30883a51288e93bca3afcf43bdcf62
Ted Wu <ted.wu@chartboost.com>
2017-03-23 18:41:19 -0700

    added flask resources

--- app/app.py
+++ app/app.py
@@ -1,5 +1,5 @@
 from flask import Flask, request
-from flask_restful import Resource, Api,
+from flask_restful import Resource, Api
 from sqlalchemy import create_engine
 
 from json import dumps
@@ -9,7 +9,7 @@ from json import dumps
 # Then import the csv file into the database run the command .import employee.csv db_name
 # 
 # Start a virtual environment 
-#
+# When you run app.py, you can curl the web address: curl http://127.0.0.1:5000 to get the data
 
 engine = create_engine('sqlite://salaries.db')
 
@@ -22,5 +22,14 @@ class HelloWorld(Resource):
 
 api.add_resource(HelloWorld, '/')
 
+
+
+class ToDoSimple(Resource):
+    def get(self):
+        return { todo_id: todos[todo_id] }
+    def put(self):
+        todos[todo_id] = request.form['data']
+        return { todo_id: todos[todo_id] }
+api.add_resource(ToDoSimple, '/<string:todo_id>')
 if __name__ == '__main__':
     app.run(debug=True)
