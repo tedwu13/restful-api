fce3cdf5c5ae22492bc94495ed1001a57c0c6092
Ted Wu <ted.wu@chartboost.com>
2017-05-08 18:18:43 -0700

    mappings

--- src/app.py
+++ src/app.py
@@ -4,6 +4,7 @@ from flask_restful import Resource, Api
 
 app = Flask(__name__)
 
+app.secret_key = 'test_secret_key'
 api = Api(app)
 #Use Api to make things easier
 
@@ -11,15 +12,16 @@ items = []
 
 class Item(Resource):
     def get(self, name):
-        for item in items:
-            if item['name'] == name:
-                return item
-        return { 'item': None }, 404
+        item = next(filter(lambda x: x['name'] == name, items), None)
+        # if the next function doesn't find anything return None
+        return { 'item': item }, 200 if item else 404
     
     def post(self, name):
         #if content type is not application/json once force is set true
         #if silent is true, it returns None
         #request.get_json(force=True, silent=True)
+        if next(filter(lambda x: x['name'] == name, items), None):
+            return { 'message': "An item with name '{}' already exists.".format(name)}
         data = request.get_json()
         item = { 'name': name, 'price': data['price'] }
         items.append(item)
@@ -33,5 +35,5 @@ class ItemList(Resource):
 api.add_resource(Item, '/item/<string:name>') #http://127.0.0.1:5000/item/itemName
 api.add_resource(ItemList, '/items')
 
-app.run(port=5000)
+app.run(port=5000, debug=True)
 
---
+++ src/security.py
@@ -0,0 +1,27 @@
+users = [
+    {
+        'id': 1,
+        'username': 'bob',
+        'password': 'asdf'
+    }
+
+]
+
+
+# two mapping dictionaries so that we can search by username or userid
+username_mapping = {
+    'bob': {
+        'id': 1,
+        'username': 'bob',
+        'password': 'asdf'
+    }
+}
+
+
+user_id_mapping = {
+    1: {
+        'id': 1,
+        'username': 'bob',
+        'password': 'asdf'
+    }
+}
